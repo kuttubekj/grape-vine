2ea96f674b8701f149ab40199775f617cd616508
kuttubekj <kutubek77@gmail.com>
2023-10-20 21:16:42 +0600

    added transaction toast

--- packages/frontend/pages/_app.tsx
+++ packages/frontend/pages/_app.tsx
@@ -4,6 +4,7 @@ import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 import Web3Provider from '@/providers/web3';
 import Layout from '@/components/Layout';
+import { Toaster } from 'react-hot-toast';
 // import { ChatUIProvider, ENV } from '@pushprotocol/uiweb';
 // import { darkChatTheme } from "@pushprotocol/uiweb";
 
@@ -19,8 +20,17 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Web3Provider>
-      {/* <ChatUIProvider theme={darkChatTheme} env={ENV.DEV}> */}
+    <main>
+      <Toaster
+        position="top-right"
+        containerClassName="mt-14 md:mt-16 lg:mt-24 max-w-screen-md mx-auto"
+        toastOptions={{
+          className: "!rounded-full select-none",
+        }}
+      />
+
+      <Web3Provider>
+        {/* <ChatUIProvider theme={darkChatTheme} env={ENV.DEV}> */}
         {/* <main
           // className={`flex min-h-screen flex-col items-center justify-between p-12 ${inter.className}`}
           className={`flex min-h-screen flex-col items-center justify-between p-12 ${inter.className}`}
@@ -30,7 +40,9 @@ export default function App({ Component, pageProps }: AppProps) {
             <Component {...pageProps} />
           </Layout>
         </main>
-      {/* </ChatUIProvider> */}
-    </Web3Provider>
+        {/* </ChatUIProvider> */}
+      </Web3Provider>
+    </main>
+
   )
 }
